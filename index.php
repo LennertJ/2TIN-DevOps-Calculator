5c0ea7d05e37f6c012cddcf89b9b787e7936c034
LennertJ <32568318+LennertJ@users.noreply.github.com>
2020-11-28 15:57:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
      <title>2TIN Devops - Calculator</title>
      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
      <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/test.style.css">
+    <link rel="stylesheet" href="assets/css/production.style.css">
     </head>
     <body>
     <h1>2TIN DevOps - Calculator</h1><hr/>
